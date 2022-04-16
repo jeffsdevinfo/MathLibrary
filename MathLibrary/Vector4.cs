dfb3d1a2804a637f7b01129878236866a85fa287
nullu5er <jeffsdevinfo@gmail.com>
2022-04-15 21:20:58 -0700

    Updated file ownership comments

--- MathLibrary/Vector4.cs
+++ MathLibrary/Vector4.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Copyright (c) 2021 Jeff Simon
+// Distributed under the MIT/X11 software license, see the accompanying
+// file license.txt or http://www.opensource.org/licenses/mit-license.php.
+
+using System;
 using Helpers = MathLibrary.Helpers;
 
 namespace MathLibrary
